92e4c84108f497bf866e3a88b8073242f9de734b
Dustin Mendoza <penguindustin@gmail.com>
2019-03-13 16:53:59 -0700

    used different template

--- simple_app/src/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'       //vue library
-import App from './App.vue' //app component
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: f => f(App),
-}).$mount('#app') // mount our application to the element with id of 'app'
