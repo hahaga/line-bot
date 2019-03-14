15fcbfa406c6e3e03376e7d9826a4db6caef8f62
Dustin Mendoza <penguindustin@gmail.com>
2019-03-13 19:01:03 -0700

    added demo flask get with vue preview

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Fortune from '@/components/Fortune'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Fortune',
+      component: Fortune
     }
   ]
 })
